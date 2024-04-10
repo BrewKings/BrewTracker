2c0c4ff9aa8ceae0ad2893feab0c21cc2492a463
Reece King <reece@ualberta.ca>
2024-04-09 18:13:28 -0600

    attached FireBase

--- arduino/firebase.py
+++ arduino/firebase.py
@@ -1,7 +1,13 @@
 import firebase_admin as fb
 
+def setup():
+    bj = fb.credentials.Certificate('google-services.json')
+    default_app = fb.initialize_app(cred_object, {
+            'https://console.firebase.google.com/project/brewtracker-278df/firestore/databases/-default-/data/~2F':databaseURL
+                })
+
 def main():
-    print("It imported")
+    setup()
 
 if __name__ == "__main__":
     main()

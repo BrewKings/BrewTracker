8f097701fd9ce74b1c16b4939646c5c1cc777272
reecemlrking <reeceking9922@gmail.com>
2024-04-14 05:33:12 +0100

    modified driver

--- arduino/driver.py
+++ arduino/driver.py
@@ -17,16 +17,21 @@ except IndexError as e:
     URL = json.load(f)["url"]["default"]
 
 
-def setup():
-    # bj = fb.credentials.Certificate('google-services.json')
-    # default_app = fb.initialize_app(cred_object, {
-    #        'https://console.firebase.google.com/project/brewtracker-278df/firestore/databases/-default-/data/~2F':databaseURL
-    #            })
+def db_setup():
+    
     fb = firebase.FirebaseApplication("https://console.firebase.google.com/project/brewtracker-1fd25/firestore/databases/-default-", None)
     result = fb.get("/data", None)
     print(result)
-    port = serial.Serial("/dev/ttyAMA0", baudrate=115200, timeout=3.0)
-    return fb 
+    return fb
+    
+def port_setup():
+    port = s.Serial("/dev/ttyAMA0", baudrate=115200, timeout=3.0)
+    return port 
+
+def read_port(port):
+    if port.in_waiting > 0:
+        ln = port.readline().decode('utf-8').rstrip()
+        return ln
 
 def loop(q):
     while(1):
@@ -46,7 +51,8 @@ def loop(q):
 
 def main():
     q = queue.Queue()
-    fb = setup()
+    #fb = db_setup()
+    port = port_setup()
     #loop(q)
     d = Data({"temp":12,"hum":44})
     q.put(d)
